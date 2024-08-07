f3fa1ea144c1c76257701c87e2b9464f97400852
=HERO+ <77572058+Arcturus1995@users.noreply.github.com>
2024-08-07 15:37:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -136,3 +136,18 @@ function sendEmail(){
   message => alert(message)
 );
 }
+VANTA.NET({
+  el: "body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x0,
+  backgroundColor: 0xFFFFFF,
+  points: 14.00,
+  maxDistance: 10.00,
+  spacing: 11.00
+})
